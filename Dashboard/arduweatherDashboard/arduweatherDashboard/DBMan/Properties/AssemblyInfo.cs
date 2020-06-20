7a2c191d3f37393c8e370a2edc1a47550239640a
Pouncer29 <bennettwlewis@hotmail.com>
2020-06-20 00:50:15 -0600

    4.7.1 and also some more projects

---
+++ Dashboard/arduweatherDashboard/arduweatherDashboard/DBMan/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DBMan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DBMan")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15E717E4-92F5-4F9E-AC08-B29A544DEE74")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dashboard/arduweatherDashboard/arduweatherDashboard/DBTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using DBMan;
+
+namespace DBTester
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var DBMan = new DBManager_Sqlite();
+        }
+    }
+}
