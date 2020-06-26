2b4c7b4030f3c488f03062a1ffdc0f7f33890358
Pouncer29 <bennettwlewis@hotmail.com>
2020-06-25 20:09:16 -0600

    Created a .net core version and moved things around

---
+++ Dashboard/ADWDBCore/DBManager/IDBManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using DBConstants;
+using Microsoft.Win32;
+
+namespace DBMan
+{
+    public interface IDBManager
+    {
+        string LatestTemperature
+        {
+            get;
+        }
+
+        string LatestHumidity
+        {
+            get;
+        }
+
+        string LatestBrightness
+        {
+            get;
+        }
+
+        string LatestWindSpeed
+        {
+            get;
+        }
+
+        string LatestWindDir
+        {
+            get;
+        }
+
+        string LatestTimestamp
+        {
+            get;
+        }
+
+        string LatestTimestampFriendly
+        {
+            get;
+        }
+        
+        event EventHandler<EventArgs> NewEntry;
+        Dictionary<DataPoint, string> GetLatestEntry();
+        string GetTime(string timestampString);
+        string SummaryString();
+    }
+}
