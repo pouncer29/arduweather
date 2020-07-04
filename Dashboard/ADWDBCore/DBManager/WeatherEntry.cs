f198b033407877a41e7ba14db128a6b0b1ff51ec
Pouncer29 <bennettwlewis@hotmail.com>
2020-07-03 21:42:16 -0600

    Angela has decied we are done. The JSON string that is being returned is proper, but it's not recongizing it as json. NO Flipflops in the house. Throw away the dead love spruce.

--- Dashboard/ADWDBCore/DBManager/WeatherEntry.cs
+++ Dashboard/ADWDBCore/DBManager/WeatherEntry.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using MongoDB.Bson;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 
 namespace DBManager.JsonHelpers
@@ -22,14 +24,13 @@ namespace DBManager.JsonHelpers
        public double? WindSpeed { get; set; }
        public string WindDir { get; set; }
 
-       
        public WeatherEntry(string timestamp)
        {
-           this.Timestamp = Timestamp;
+           this.Timestamp = timestamp;
        }
 
        public WeatherEntry(string timestamp, double? temp = null, double? humid = null, double? brightness = null,
-           double? windSpeed = null, string windDir = null)
+           double? windSpeed = null, string windDir = null):this(timestamp)
        {
            this.Timestamp = timestamp;
            this.TEMPERATURE = temp;
@@ -38,18 +39,85 @@ namespace DBManager.JsonHelpers
            this.WindSpeed = windSpeed;
            this.WindDir = windDir;
        }
+
+       /// <summary>
+       /// Gets the weather deets
+       /// </summary>
+       /// <returns></returns>
+       /// <exception cref="InvalidOperationException"></exception>
+       public List<object> GetDataValues()
+       {
+            var deets = new List<object>();
+            if (string.IsNullOrEmpty(this.Timestamp))
+            {
+                throw new InvalidOperationException("Cannot create chart without timestamp");
+            }
+            deets.Add(Timestamp);
+ 
+            if (this.TEMPERATURE != null)
+            {
+                deets.Add(this.TEMPERATURE);
+            }
+ 
+            if (this.Humidity != null)
+            {
+                deets.Add(this.Humidity); 
+            }
+ 
+            if (this.WindSpeed != null)
+            {
+                deets.Add(this.WindSpeed);
+            }
+ 
+            if (this.Brightness != null)
+            {
+                deets.Add(this.Brightness);
+            }
+ 
+            if (deets.Count < 2 )
+            {
+                throw new InvalidOperationException("Cannot Create Chart with less than 2 axis");
+            }
+ 
+            return deets;          
+       }
+       public static List<string> GetHeaderList()
+       {
+           var headers = new List<string>();
+           headers.Add(DBConstants.DBDeets.TimeKey);
+           headers.Add("Temperature");
+           headers.Add("Humidity"); 
+           headers.Add("Wind Speed");
+           headers.Add("Brightness");
+           return headers;
+
+       }
     }
 
     public class Weather_Chart
     {
-        public static List<WeatherEntry> GetDummyChart()
+        public static List<object> ToGChartsArray(List<WeatherEntry> entries)
+        { 
+            var gChartsArray = new List<object>();
+           gChartsArray.Add(WeatherEntry.GetHeaderList());
+           entries.ForEach(entry =>
+           {
+               gChartsArray.Add(entry.GetDataValues());
+           });
+
+           return gChartsArray;
+        }
+        public static List<object> GetDummyChart()
         {
             var timestampToday = DateTime.Today.ToString();
             var timestampYesterday = DateTime.Today.AddDays(-1).ToString();
             var testList = new List<WeatherEntry>();
-            testList.Add(new WeatherEntry(timestampYesterday){Humidity = 40.02});
-            testList.Add(new WeatherEntry(timestampToday){Humidity = 15.02});
-            return testList;
+            testList.Add(new WeatherEntry(timestampYesterday, 5, 2, 6, 8, "W"));
+            testList.Add(new WeatherEntry(timestampYesterday, 3, 11, 18, 8, "E"));
+
+            var gChartData = ToGChartsArray(testList);
+            
+            return gChartData;
         }
     } 
 }
